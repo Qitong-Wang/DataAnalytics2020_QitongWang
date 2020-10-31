f2b7849e2ea8778a83872e80f991ef32fda70d16
Tony <wqtls1998@gmail.com>
2020-10-30 20:00:01 -0400

    Oct 30

---
+++ lab5/lab5.R
@@ -0,0 +1,77 @@
+require(kknn)
+data(iris)
+m <- dim(iris)[1]
+val <- sample(1:m, size = round(m/3), replace = FALSE, prob = rep(1/m, m)) 
+iris.learn <- iris[-val,] 	# train
+iris.valid <- iris[val,]	# test
+iris.kknn <- train.kknn(Species~., iris.learn, distance = 1, kernel = c("triangular", "epanechnikov", "biweight", "triweight", "cos", "inv", "gaussian", "rank", "optimal") )
+summary(iris.kknn)
+table(predict(iris.kknn,iris.valid),iris.valid$Species)
+
+head(iris.kknn$fitted.values)
+
+
+require(randomForest)
+fitKF <- randomForest(Kyphosis ~ Age + Number + Start,   data=kyphosis)
+print(fitKF) 	# view results
+importance(fitKF) # importance of each predictor
+#
+fitSwiss <- randomForest(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+print(fitSwiss) # view results
+importance(fitSwiss) # importance of each predictor
+varImpPlot(fitSwiss)
+
+plot(fitSwiss)
+
+getTree(fitSwiss,1, labelVar=TRUE)
+
+help(randomForest) # look at all the package contents and the randomForest method options
+
+# look at rfcv - random forest cross-validation - 
+help(rfcv)
+
+# other data....
+data(imports85)
+
+# perform randomForest and other tree methods.....
+
+require(rpart)
+swiss_rpart <- rpart(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+plot(swiss_rpart) # try some different plot options
+text(swiss_rpart) # try some different text options
+
+
+# Regression Tree Example
+require(rpart)
+# build the  tree
+fitM <- rpart(Mileage~Price + Country + Reliability + Type, method="anova", data=cu.summary)
+printcp(fitM) # display the results
+plotcp(fitM)
+summary(fitM)
+par(mfrow=c(1,2)) 
+rsq.rpart(fitM) # visualize cross-validation results
+# plot tree
+plot(fitM, uniform=TRUE, main="Regression Tree for Mileage ")
+text(fitM, use.n=TRUE, all=TRUE, cex=.8)
+# prune the tree
+pfitM<- prune(fitM, cp=0.01160389) # from cptable??? adjust this to see the effect
+# plot the pruned tree
+plot(pfitM, uniform=TRUE, main="Pruned Regression Tree for Mileage")
+text(pfitM, use.n=TRUE, all=TRUE, cex=.8)
+
+fitK <- rpart(Kyphosis ~ Age + Number + Start, method="class", data=kyphosis)
+printcp(fitK) # display the results
+plotcp(fitK) # visualize cross-validation results
+summary(fitK) # detailed summary of splits
+# plot tree
+plot(fitK, uniform=TRUE, main="Classification Tree for Kyphosis")
+text(fitK, use.n=TRUE, all=TRUE, cex=.8)
+
+     
+pfitK<- prune(fitK, cp=   fitK$cptable[which.min(fitK$cptable[,"xerror"]),"CP"])
+plot(pfitK, uniform=TRUE, main="Pruned Classification Tree for Kyphosis")
+text(pfitK, use.n=TRUE, all=TRUE, cex=.8)
+  
+     
+
+
---
+++ lab6/lab6.R
@@ -0,0 +1,49 @@
+require(randomForest)
+fitKF <- randomForest(Kyphosis ~ Age + Number + Start,   data=kyphosis)
+print(fitKF) 	# view results
+importance(fitKF) # importance of each predictor
+#
+fitSwiss <- randomForest(Fertility ~ Agriculture + Education + Catholic, data = swiss)
+print(fitSwiss) # view results
+importance(fitSwiss) # importance of each predictor
+varImpPlot(fitSwiss)
+
+plot(fitSwiss)
+
+sgetTree(fitSwiss,1, labelVar=TRUE)
+
+help(randomForest) # look at all the package contents and the randomForest method options
+
+# look at rfcv - random forest cross-validation - 
+help(rfcv)
+
+# other data....
+data(imports85)
+
+
+library(ISLR)
+library(MASS)
+library(boot)
+set.seed(1)
+
+# read the documentation for sample() function
+help("sample")
+train = sample(392,196)
+lm.fit <- lm(mpg~horsepower, data = Auto, subset = train)
+attach(Auto)
+mean((mpg-predict(lm.fit,Auto))[-train]^2)
+
+lm.fit2 <- lm(mpg~poly(horsepower,2), data = Auto, subset = train) # Quadratic 
+mean((mpg-predict(lm.fit2,Auto))[-train]^2) 
+
+lm.fit3 <- lm(mpg~poly(horsepower,3), data = Auto, subset = train) # Cubic
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
+
+set.seed(2)
+train = sample(392,196)
+lm.fit <- lm(mpg~horsepower, data = Auto, subset = train)
+mean((mpg-predict(lm.fit,Auto))[-train]^2)
+lm.fit2 <- lm(mpg~poly(horsepower,2), data = Auto, subset = train) # Quadratic 
+mean((mpg-predict(lm.fit2,Auto))[-train]^2) 
+lm.fit3 <- lm(mpg~poly(horsepower,3), data = Auto, subset = train) # Cubic
+mean((mpg-predict(lm.fit3,Auto))[-train]^2)
